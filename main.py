f1f97559af22467525214d8dab566b4eb1fb841b
SmartConTractor <login.blockchain888@gmail.com>
2025-03-25 14:04:33 +0100

    Get data from user input as strings.

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
-from flask import Flask, render_template
-#import requests
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 # main page
-@app.route("/")
+@app.route("/", methods = ["GET","POST"])
 def index():
+    
+    if request.method == "POST":
+        first_name = request.form["first_name"]
+        last_name = request.form["last_name"]
+        e_mail = request.form["e_mail"]
+        date = request.form["start_date"]
+        occupation = request.form["occupation"]
+        print(first_name)
+        print(last_name)
+        print(date)
+        print(e_mail)
+        print(occupation)
+        
+        
     return render_template("index.html")
 
 
